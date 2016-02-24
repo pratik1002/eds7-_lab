4dbb488038b187b69ce05a9bd8a4de58eaccfa63
unknown <pratikorissa@gmail.com>
2016-02-24 17:04:13 +0530

    second commit

---
+++ systick2.c
@@ -0,0 +1,79 @@
+
+#define NVIC_ST_CTRL_R          (*((volatile unsigned long *)0xE000E010))
+#define NVIC_ST_RELOAD_R        (*((volatile unsigned long *)0xE000E014))
+#define NVIC_ST_CURRENT_R       (*((volatile unsigned long *)0xE000E018))	
+#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
+#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
+#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
+#define GPIO_PORTF_ODR_R        (*((volatile unsigned long *)0x4002550C))
+#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
+#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
+#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
+#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
+#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
+#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
+#define GPIO_LOCK_KEY           0x4C4F434B                                 // Unlocks the GPIO_CR register
+#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
+
+	
+void SysTick_Init(void);
+void SysTick_Wait(unsigned long delay);
+void SysTick_Init(void);
+void PortF_Init(void);
+unsigned long led;
+
+
+
+
+
+
+void PortF_Init(void){ volatile unsigned long delay;
+  SYSCTL_RCGC2_R |= 0x00000020;     // 1) activate clock for Port F
+  delay = SYSCTL_RCGC2_R;           // allow time for clock to start
+  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock GPIO Port F
+	GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0
+  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog on PF
+  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) PCTL GPIO on PF4-0
+  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 in, PF3-1 out
+  GPIO_PORTF_AFSEL_R = 0x00;        // 6) disable alt funct on 
+  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital I/O on PF4-0
+}
+
+void SysTick_Init(void){
+  NVIC_ST_CTRL_R = 0;                   // disable SysTick during setup
+  NVIC_ST_RELOAD_R = 0x00FFFFFF;        // maximum reload value
+  NVIC_ST_CURRENT_R = 0;                // any write to current clears it             
+  NVIC_ST_CTRL_R = 0x00000005;          // enable SysTick with core clock
+}
+
+
+void SysTick_Wait(unsigned long delay)
+{ NVIC_ST_RELOAD_R = delay-1;
+	NVIC_ST_CURRENT_R = 0;
+	while((NVIC_ST_CTRL_R&0x00010000)==0)
+	{
+	}
+}
+
+
+void SysTick_Wait10ms(unsigned long delay){
+  unsigned long i;
+  for(i=0; i<delay; i++){
+    SysTick_Wait(24000);  // wait 10ms (assumes 50 MHz clock)
+  }}
+
+	
+	
+	int main(void)
+	{
+		PortF_Init();
+		SysTick_Init();
+		while(1)
+		{
+			led=GPIO_PORTF_DATA_R;
+			led=led^0x02;
+			GPIO_PORTF_DATA_R=led;
+			SysTick_Wait10ms(1);
+		}}
+		
+		
